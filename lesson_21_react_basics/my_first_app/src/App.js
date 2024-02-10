a762eb0e6fb88fc7652c1a9331dd9bd0c6d37d80
Eugen Grünwald <eugen.13.70.6@gmail.com>
2024-02-10 19:29:00 +0100

    my_first_app

---
+++ lesson_21_react_basics/my_first_app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import tree_img from './media/tree.jpeg';
+
+function App() {
+  return (
+    <div>
+      <h2>Заголовок11111</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quam in vel amet quod nemo harum rerum. Enim libero ipsa accusantium porro tempore provident voluptatum hic vero reprehenderit, labore ducimus.</p>
+      <img src={tree_img} alt='text'/>
+    </div>
+  );
+}
+
+export default App;
+
+
+// JSX - синтаксическое расширение, которое позволяет использовать html-разметку в JS
